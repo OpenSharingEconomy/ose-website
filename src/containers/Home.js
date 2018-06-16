9198e9f2a9c009ab0a8135b59c8a905706e64e63
TitouanLarnicol <larnicol.titouan@orange.fr>
2018-06-16 09:04:13 +0200

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { withSiteData,SiteData} from 'react-static'
+import index from '../index.jpeg'
+
+export default withSiteData(() => (
+  <div>
+    <h1 style={{ textAlign: 'center' }}>Welcome to Open Sharing Economy</h1>
+
+  </div>
+))
