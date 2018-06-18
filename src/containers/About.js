f35052e405371c359ea7deb37bd9cbc2b469d60f
TitouanLarnicol <larnicol.titouan@orange.fr>
2018-06-18 23:27:52 +0200

    font & logo

--- src/containers/About.js
+++ src/containers/About.js
@@ -5,6 +5,5 @@ import React from 'react'
 export default () => (
   <div id='about'>
     <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
   </div>
 )
--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -3,6 +3,9 @@ import ScrollIntoView from 'react-scroll-into-view'
 import { Router, Route} from 'react-static'
 import './Header.css'
 import index from '../../index.jpeg'
+import telegramme from '../../telegrame.jpg'
+import twitter from '../../twitter.png'
+
 import * as Scroll from 'react-scroll';
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
@@ -22,6 +25,14 @@ export default class Header extends React.Component {
             <Link activeClass="active" className="whitepaper" to="whitepaper" spy={true} smooth={true} duration={750} offset={-200}> WhitePaper </Link>
             <Link activeClass="active" className="about" to="about" spy={true} smooth={true} duration={750} offset={200}> About  </Link>
           </nav>
+          <div id="ose-icon">
+            <a href="   https://t.me/blockfood" >
+            <img className="icon" src={telegramme} />
+            </a>
+            <a href="https://twitter.com/BlockFood" >
+              <img  className="icon"  src={twitter} />
+            </a>
+          </div>
         </div>
       </header>
 
