dbfc3ac02589bf517ed260cae03ee79e51baade7
TitouanLarnicol <larnicol.titouan@orange.fr>
2018-06-28 14:54:38 +0200

    responsive in progress

---
+++ src/containers/RoadMap.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { withSiteData,SiteData} from 'react-static'
+
+export default withSiteData(() => (
+  <div id='roadmap'>
+    <h1 style={{ textAlign: 'center' }}>ROADMAP </h1>
+    <p> OK </p>
+  </div>
+))
--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -9,14 +9,22 @@ import twitter from '../../img/twitter.png'
 import { Prompt } from 'react-router'
 import * as Scroll from 'react-scroll';
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem,Button } from 'reactstrap';
 
 
 export default class Header extends React.Component {
   constructor(props){
     super(props)
-    this.state = {style:"Fix",width:2500}
+    this.state = {style:"Fix",width:2500,dropdownOpen: false}
     this.handleScroll = this.handleScroll.bind(this)
     this.handleSize = this.handleSize.bind(this)
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
   }
 
   componentDidMount(){
@@ -48,13 +56,19 @@ export default class Header extends React.Component {
           <header  id="headerDisplay" refs="headerDisplay" className={`row align-items-center ${headerAnim}`}>
               <div className="col-8 menuDisplay">
                 <img className={oseLogo} src={Oselogo} onClick={() => scroll.scrollToTop()} />
-                {Width<515 ?
-                    (<nav id="header-menu">
-                      <Link activeClass="active" className="link" to="introduction" spy={true} smooth={true} duration={750} offset={-200}> Home </Link>
-                      <Link activeClass="active" className="link" to="team" spy={true} smooth={true} duration={750} offset={-200}> Team </Link>
-                      <Link activeClass="active" className="link" to="whitepaper" spy={true} smooth={true} duration={750} offset={-200}> WhitePaper </Link>
-                      <Link activeClass="active" className="link" to="about" spy={true} smooth={true} duration={750} offset={200}> About </Link>
-                    </nav>
+                {Width<950?
+                    (<Dropdown id="menu-dropdown" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle style={{backgroundColor:'white',color:'black'}}>
+          <p>Menu</p>
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem  href="#introduction" > Home </DropdownItem>
+          <DropdownItem  href="#team" > team</DropdownItem>
+          <DropdownItem  href="#whitepaper"> White Paper</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem  href="#whitelist">WHITELIST</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
                   )
                   :
                   (<nav id="header-menu">
@@ -66,9 +80,11 @@ export default class Header extends React.Component {
                 }
               </div>
               <div className="col-4 iconDisplay">
-                <div id="button-whitelist">
-                  <Link activeClass="active" className="whitelist" to="whitelist" spy={true} smooth={true} duration={750} offset={-200}> WHITELIST </Link>
-                </div>
+                {Width>950?
+                (  <div id="button-whitelist">
+                    <Link activeClass="active" className="whitelist" to="whitelist" spy={true} smooth={true} duration={750} offset={-200}> WHITELIST </Link>
+                  </div> ): null
+                }
                 <div id="oseIcon">
                   <a href="   https://t.me/blockfood" >
                     <img className="icon" src={telegramme} />
